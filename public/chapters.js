bb68b551c55d9f4f33aae9e5f3fb332302ef8968
Azreen Latiff <azreen@quickschools.com>
2013-10-22 21:44:39 -0400

    Finessing apikeys and oauth sections

--- public/chapters.js
+++ public/chapters.js
@@ -2,4 +2,6 @@ var chapters = [];
 
 chapters[chapters.length] = "01-intro.html";
 chapters[chapters.length] = "02-resources.html";
-chapters[chapters.length] = "10-showcaseapps.html";
+chapters[chapters.length] = "04-apikeys.html";
+chapters[chapters.length] = "05-oauth.html";
+//chapters[chapters.length] = "10-showcaseapps.html";
